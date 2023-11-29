a412a3b2dfc8fa8bab3e3b13fd39017be5dc4570
Marco Andrade <marcoand@mit.edu>
2023-11-29 00:10:22 -0500

    Tweaks

--- hdl/fixed_point_gen.py
+++ hdl/fixed_point_gen.py
@@ -17,23 +17,16 @@ for i in range(181):
   sin_val = math.sin(rad)
   if (i != 360):
     fixed_sin_val = float_to_fixed(sin_val,16)
-  if (i == 0 or i == 360):
-    print(f"8'd{i}: amp_out <= 32'b00000000000000000000000000000000")
+  if (i == 0 or i == 360 or i == 180):
+    print(f"8'd{i}: amp_out <= 32'b00000000000000000000000000000000;")
   elif (i == 90):
-    print(f"8'd{i}: amp_out <= 32'b00000000000000010000000000000000")
+    print(f"8'd{i}: amp_out <= 32'b00000000000000010000000000000000;")
   elif (i == 270):
-    print(f"8'd{i}: amp_out <= 32'b11111111111111110000000000000000")
+    print(f"8'd{i}: amp_out <= 32'b11111111111111110000000000000000;")
   elif (i < 180):
-    string = f"0000000000000000{bin(fixed_sin_val)[2:]}"
+    string = f"0000000000000000{bin(fixed_sin_val)[2:]};"
     for j in range(16 - (len(bin(fixed_sin_val)) - 2)):
       string = '0' + string
     print(f"8'd{i}: amp_out <= 32'b{string}")
-  else:
-    string = f"{bin(fixed_sin_val)[2:]}"
-    for j in range(16 - (len(bin(fixed_sin_val)[2:]))):
-      if j == 16 - (len(bin(fixed_sin_val)[2:])) - 1:
-        string = '1' + string
-      else:
-        string = '0' + string
-    print(f"8'd{i}: amp_out <= 32'b1111111111111111{string}")
+
     
