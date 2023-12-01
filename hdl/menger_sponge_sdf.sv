957fda4b51f22e1cdaf7b4bd177adcda97ab27ef
Ben Hunsberger <benhunsberger13@hotmail.com>
2023-12-01 12:43:45 -0500

    fixed menger sponge syntax errors

--- hdl/menger_sponge_sdf.sv
+++ hdl/menger_sponge_sdf.sv
@@ -1,9 +1,6 @@
 `timescale 1ns / 1ps
 `default_nettype none
 
-parameter BITS = 32;
-parameter FIXED = 16;
-
 
 // VECTOR FUNCTIONS
 
@@ -23,14 +20,6 @@ function vec3 vec3_sub;
         vec3_sub.y = a.y - b.y;
         vec3_sub.z = a.z - b.z;
     end
-endfunction
-
-// Absolute value of number to run vector coords through
-function logic signed [BITS-1:0] abs;
-  input logic signed [BITS-1:0] a;
-  begin
-    abs = a[BITS-1] ? ~a + 1 : a;
-  end
 endfunction
 
 // Produces a new vector of the absolute value of vector v
@@ -59,29 +48,13 @@ function logic signed [BITS-1:0] signed_maximum;
   input logic signed [BITS-1:0] b;
   begin
     if(a[BITS-1] && !b[BITS-1]) begin
-      signed_minimum = b;
+      signed_maximum = b;
     end else if(!a[BITS-1] && b[BITS-1]) begin
-      signed_minimum = a;
+      signed_maximum = a;
     end else if(a[BITS-1] && b[BITS-1]) begin
-      signed_minimum = a > b ? b : a;
+      signed_maximum = a > b ? b : a;
     end else begin
-      signed_minimum = a < b ? b : a;
-    end
-  end
-endfunction
-
-function logic signed [BITS-1:0] signed_minimum;
-  input logic signed [BITS-1:0] a;
-  input logic signed [BITS-1:0] b;
-  begin
-    if(a[BITS-1] && !b[BITS-1]) begin
-      signed_minimum = a;
-    end else if(!a[BITS-1] && b[BITS-1]) begin
-      signed_minimum = b;
-    end else if(a[BITS-1] && b[BITS-1]) begin
-      signed_minimum = a > b ? a : b;
-    end else begin
-      signed_minimum = a < b ? a : b;
+      signed_maximum = a < b ? b : a;
     end
   end
 endfunction
@@ -100,27 +73,27 @@ endfunction
 function vec3 trans;
     input vec3 p;
     input logic [BITS-1:0] scale;
-    logic vec3 scale_vec;
+    vec3 scale_vec;
     logic signed [BITS-1:0] prev_x;
-    logic signed [BITS-1:0] prev_y;
+    logic signed [BITS-1:0] prev_z;
     begin
         scale_vec.x = scale;
         scale_vec.y = scale;
         scale_vec.z = scale;
 
         p = vec3_sub(abs_vec3(p), scale_vec);
-        p.x = p.x * -1;
-        p.y = p.y * -1;
-        p.z = p.z * -1;
+        p.x = p.x * to_fixed(-1);
+        p.y = p.y * to_fixed(-1);
+        p.z = p.z * to_fixed(-1);
 
         prev_x = p.x;
-        p.x = ((p.x - p.y > 0.) ? p.y : p.x);
-        p.y = ((prev_x - p.y > 0.) ? prev_x : p.y);
+        p.x = ((p.x - p.y > 0) ? p.y : p.x);
+        p.y = ((prev_x - p.y > 0) ? prev_x : p.y);
         prev_z = p.z;
-        p.z = ((p.z - p.y > 0.) ? p.y : p.z);
-        p.y = ((prev_z - p.y > 0.) ? prev_z : p.y);
+        p.z = ((p.z - p.y > 0) ? p.y : p.z);
+        p.y = ((prev_z - p.y > 0) ? prev_z : p.y);
 
-        p.y = (abs(p.y-0.5*s)-0.5*s);
+        p.y = (abs(p.y-0.5*scale)-0.5*scale);
 
         return p;    
     end
@@ -167,11 +140,11 @@ module sdf (
 );
   typedef enum {IDLE=0, TRANS=1, DIV=2, INIT_Q=3, LEN_Q=4, SDF_BOX=5, FINAL_DIV=6, DONE=7} sdf_state;
   sdf_state state;
-  logic vec3 p;
+  vec3 p;
   logic [BITS-1:0] scale;
-  logic vec3 half_vec;
+  vec3 half_vec;
   logic [BITS-1:0] p_scale;
-  logic vec3 q;
+  vec3 q;
   logic signed [BITS-1:0] sdBox;
   logic [BITS-1:0] length_q;
 
@@ -230,9 +203,9 @@ module sdf (
     .val(div_b_out)
   );
 
-  assign sdf_red_out <= 8'hF0;
-  assign sdf_green_out <= 8'h00;
-  assign sdf_blue_out <= 8'h00;
+  assign sdf_red_out = 8'hF0;
+  assign sdf_green_out = 8'h00;
+  assign sdf_blue_out = 8'h00;
   
   always_ff @(posedge clk_in) begin
     if(rst_in) begin
@@ -244,9 +217,9 @@ module sdf (
       IDLE: begin
         if(sdf_start) begin
           scale <= to_fixed(260);
-          half_vec.x <= to_fixed(.5);
-          half_vec.y <= to_fixed(.5);
-          half_vec.z <= to_fixed(.5);
+          half_vec.x <= to_fixed(0.5);
+          half_vec.y <= to_fixed(0.5);
+          half_vec.z <= to_fixed(0.5);
           p.x <= x * scale;
           p.y <= y * scale;
           p.z <= z * scale;
@@ -271,7 +244,7 @@ module sdf (
           end
       end
       INIT_Q: begin
-        q <= vec3_sub(abs_vec3(p) - half_vec);
+        q <= vec3_sub(abs_vec3(p), half_vec);
         state <= LEN_Q;
         sqrt_in <= (q.x*q.x + q.y*q.y + q.z*q.z);
         sqrt_start <= 1;
